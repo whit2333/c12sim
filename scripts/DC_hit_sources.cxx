09990e800e7b0884761865805b84e6065cd1f619
Whitney Armstrong <warmstrong@anl.gov>
2016-02-17 10:52:34 -0600

    modified:   scripts/DC_hit_sources.cxx

--- scripts/DC_hit_sources.cxx
+++ scripts/DC_hit_sources.cxx
@@ -10,12 +10,31 @@
 #include "TTree.h"
 #include "THStack.h"
 
+
+TH1F * get_hist_from_map(std::map<int,TH1F*>& hist_map, int pid){
+   if(hist_map.count(pid) == 0) {
+   std::cout << " pid" << pid << std::endl;
+      hist_map[pid] = new TH1F(Form("fhZ_%d",pid), Form("Z - %d",pid), 300,-10,600);
+   }
+   return hist_map[pid];
+}
+
+
 int DC_hit_sources(
       int run_number = 0
       )
 {
 
-   std::vector<int> runs = { 300, 307, 309 , 303, 304, 305, 308, 306 };
+   std::vector<int> runs;// = { 300 , 307, 309 };//, 303, 304, 305, 308, 306 };
+   for(int i=300; i<320;i++) {
+      runs.push_back(i);
+   }
+   //std::vector<int> runs;// = { 300 , 307, 309 };//, 303, 304, 305, 308, 306 };
+   for(int i=400; i<420;i++) {
+      runs.push_back(i);
+   }
+
+   std::map<int,TH1F*> pid_hists;
 
    TChain * t =  new TChain("clasdigi_hits");
    for(auto i : runs) {
@@ -52,9 +71,12 @@ int DC_hit_sources(
    //std::array<std::array<std::array<std::array<int,112>,6>,6>,6> nWireHitsTotal;
    //std::array<std::array<std::array<std::array<int,112>,6>,6>,6> nWireHitsSquaredTotal;
 
-   TH1F * fhZ              = new TH1F("fhZ", "Z", 100,-10,600);
-   TH2F * fhRvsZ           = new TH2F("fhRvsZ", "r vs Z", 100,-10,600,100,0,400);
-   TH2F * fhWireNumbervsZ  = new TH2F("fhWireNumbervsZ", "wire vs Z", 100,-10,600,112,1,113);
+   TH1F * fhZ              = new TH1F("fhZ", "Z", 300,-10,600);
+   TH2F * fhRvsZ           = new TH2F("fhRvsZ", "r vs Z", 300,-10,600,200,0,400);
+   TH2F * fhWireNumbervsZ  = new TH2F("fhWireNumbervsZ", "wire vs Z", 300,-10,600,112,1,113);
+   TH1F * fhZ2              = new TH1F("fhZ2", "mother Z", 300,-10,600);
+   TH2F * fhRvsZ2           = new TH2F("fhRvsZ2", "mother r vs Z", 300,-10,600,200,0,400);
+   TH2F * fhWireNumbervsZ2  = new TH2F("fhWireNumbervsZ2", "mother wire vs Z", 300,-10,600,112,1,113);
    //fAvgNhitsVsWireAll      = new TH2F("fAvgNhitsVsWireAll", "Avg Nhits Vs Wire all",
    //      112,1,113,100,0,0.01);
 
@@ -89,22 +111,33 @@ int DC_hit_sources(
             int lay     = ahit->fDCWire.fLayer;
             int wire    = ahit->fDCWire.fWire;
             int bin     = wire + (ahit->fDCWire.fLayer-1)*112;
+            int pid     = ahit->fPDGCode;
+
 
             TParticle * part = 0;
+            TParticle * orig_part = 0;
             int ntraj = traj_verts->GetEntries();
             for(int ipart = 0; ipart<ntraj; ipart++){
                TParticle * avert = (TParticle*)((*traj_verts)[ipart]);
-               if(avert->GetSecondMother() == trk_id) {
+               if(avert->GetFirstMother() == trk_id) {
                   part = avert;
-                  break;
+               }
+               if(avert->GetSecondMother() == trk_id) {
+                  orig_part = avert;
                }
             }
             if(part) {
-
+               TH1F * pid_hist = get_hist_from_map(pid_hists,pid);
+               pid_hist->Fill(part->Vz());
                fhRvsZ->Fill(part->Vz(),part->R());
                fhZ->Fill(part->Vz());
-
                fhWireNumbervsZ->Fill(part->Vz(),wire);
+            }
+            if(orig_part) {
+
+               fhRvsZ2->Fill(orig_part->Vz(),orig_part->R());
+               fhZ2->Fill(orig_part->Vz());
+               fhWireNumbervsZ2->Fill(orig_part->Vz(),wire);
             }
 
             //nWireHits[sec-1][sl-1][lay-1][wire-1]++;
@@ -183,7 +216,56 @@ int DC_hit_sources(
    c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_wirevsZ_%d.png",aNumber));
    c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_wirevsZ_%d.pdf",aNumber));
 
+   //---------------------------------------------------------
+   //
+   c0 = new TCanvas();
+   c0->cd();
+   fhZ2->Draw("colz");
+   //mathtex.DrawMathText(0.4,0.83,"\\mathscr{L} = 1.3 \\times 10^{35} [cm^{-1}s^{-1}]");
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_Z2_%d.png",aNumber));
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_Z2_%d.pdf",aNumber));
+
+   //---------------------------------------------------------
+   //
+   c0 = new TCanvas();
+   c0->cd();
+   fhRvsZ2->Draw("colz");
+   //mathtex.DrawMathText(0.4,0.83,"\\mathscr{L} = 1.3 \\times 10^{35} [cm^{-1}s^{-1}]");
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_RvsZ2_%d.png",aNumber));
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_RvsZ2_%d.pdf",aNumber));
+
+   //---------------------------------------------------------
+   //
+   c0 = new TCanvas();
+   c0->cd();
+   fhWireNumbervsZ2->Draw("colz");
+   //mathtex.DrawMathText(0.4,0.83,"\\mathscr{L} = 1.3 \\times 10^{35} [cm^{-1}s^{-1}]");
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_wirevsZ2_%d.png",aNumber));
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_wirevsZ2_%d.pdf",aNumber));
+
    std::cout << " N events : " << nEvents << std::endl;
+
+   c0 = new TCanvas();
+   c0->cd();
+   int icol = 0;
+   THStack * hs = new THStack("z for pids","");
+   TLegend * leg = new TLegend(0.7,0.7,0.9,0.9);
+   for(auto hp : pid_hists ) {
+      hp.second->SetLineColor(colors[icol]);
+      hp.second->SetLineWidth(2);
+      leg->AddEntry(hp.second, Form("%d",hp.first),"l");
+      hs->Add(hp.second);
+      icol++;
+   }
+   hs->Draw("nostack");
+   leg->Draw();
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_pid_Z_%d.png",aNumber));
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_pid_Z_%d.pdf",aNumber));
+
+   gPad->SetLogy(true);
+   c0->Update();
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_pid_Z_log_%d.png",aNumber));
+   c0->SaveAs(Form("data/results/DC_hit_sources/DC_hit_sources_pid_Z_log_%d.pdf",aNumber));
 
    return 0;
 }
